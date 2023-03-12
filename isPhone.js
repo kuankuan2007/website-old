544253d3b576baaaa09cd784fd5aa606fc4f70a3
kuankuan2007 <2163826131@qq.com>
2023-03-12 21:21:17 +0800

    手机导航栏兼容

---
+++ isPhone.js
@@ -0,0 +1,9 @@
+function isPhone(){
+    if (userAgent=navigator.userAgent.toLowerCase().indexOf("mobile")!=-1){
+        try{document.getElementById("nav").setAttribute("id","phonenav")
+            document.getElementById("helpSonNav").setAttribute("id","helpSonPhoneNav")}
+        finally{
+            
+        }
+    }
+}
