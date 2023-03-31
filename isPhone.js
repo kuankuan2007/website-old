6639d7db76398d0f703101e345b5b67fa1b088b8
kuankuan2007 <2163826131@qq.com>
2023-03-31 22:19:43 +0800

    nav 伪元素

--- isPhone.js
+++ isPhone.js
@@ -7,3 +7,12 @@ function isPhone(){
         }
     }
 }
+function asPhone(){
+    if (userAgent=navigator.userAgent.toLowerCase().indexOf("mobile")!=-1){
+        try{document.getElementById("nav").setAttribute("id","phonenav")
+            document.getElementById("helpSonNav").setAttribute("id","helpSonPhoneNav")}
+        finally{
+            
+        }
+    }
+}
